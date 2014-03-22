b8ff2d5cbe77ba0c385c736db0e570ccbaa32f07
Hor Phallin <hor_phallin@yahoo.com>
2014-03-22 16:12:22 +0700

    Translate and include font

---
+++ app/lang/en/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| General translation
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default error messages used by
+	| the validator class. Some of these rules have multiple versions such
+	| as the size rules. Feel free to tweak each of these messages here.
+	|
+	*/
+
+	"login" => "Login",
+	"logout" => "Logout",
+	"register" => "Register",
+
+);
